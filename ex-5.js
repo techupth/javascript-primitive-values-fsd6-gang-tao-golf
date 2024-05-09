fdc8b5d7b1593ea5e2e5af62d5ed4159c54fbb12
fossi13 <fossi13@users.noreply.github.com>
2024-05-09 13:10:33 +0700

    finish primitive

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,15 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+
+let price = 27000;
+let count = 0.25;
+
+let total = price * count;
+
+let totalPrice = 0;
+
+totalPrice = price - total;
+
+console.log(total);
+console.log(totalPrice);
